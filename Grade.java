616d53daa7da6f373b2807b5b42ca2c3b69ea06c
shreyaa2122 <shreya.23bcy10243@vitbhopal.ac.in>
2025-09-25 16:26:24 +0530

    Update Grade.java

--- Grade.java
+++ Grade.java
@@ -1,5 +1,30 @@
 package edu.ccrm.domain;
 
-public enum Grade {
+public class Grade {
+    private String studentId;
+    private String courseCode;
+    private double mark;
 
+    public Grade(String studentId, String courseCode, double mark) {
+        this.studentId = studentId;
+        this.courseCode = courseCode;
+        this.mark = mark;
+    }
+
+    public String getStudentId() { return studentId; }
+    public String getCourseCode() { return courseCode; }
+    public double getMark() { return mark; }
+
+    public void setStudentId(String studentId) { this.studentId = studentId; }
+    public void setCourseCode(String courseCode) { this.courseCode = courseCode; }
+    public void setMark(double mark) { this.mark = mark; }
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+               "studentId='" + studentId + '\'' +
+               ", courseCode='" + courseCode + '\'' +
+               ", mark=" + mark +
+               '}';
+    }
 }
