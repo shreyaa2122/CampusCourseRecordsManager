ffcae4186f42fb3c0972585846dae8617e1ab5ab
shreyaa2122 <shreya.23bcy10243@vitbhopal.ac.in>
2025-09-25 16:25:31 +0530

    Update CourseService.java

--- CourseService.java
+++ CourseService.java
@@ -1,5 +1,25 @@
 package edu.ccrm.service;
 
-public class CourseService {
+import edu.ccrm.domain.Course;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class CourseService {
+    private List<Course> courses = new ArrayList<>();
+
+    public void addCourse(Course course) {
+        courses.add(course);
+        System.out.println("Course added successfully: " + course.getTitle());
+    }
+
+    public List<Course> getAllCourses() {
+        return new ArrayList<>(courses);
+    }
+
+    public List<Course> findCoursesByDepartment(String department) {
+        return courses.stream()
+                      .filter(course -> course.getDepartment().equalsIgnoreCase(department))
+                      .collect(Collectors.toList());
+    }
 }
