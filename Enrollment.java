904c1cf976b6f4dcae5ede8aac703bc5b580c6a2
shreyaa2122 <shreya.23bcy10243@vitbhopal.ac.in>
2025-09-25 16:23:20 +0530

    Update Enrollment.java

--- Enrollment.java
+++ Enrollment.java
@@ -1,5 +1,36 @@
 package edu.ccrm.domain;
 
 public class Enrollment {
+    private String studentId;
+    private String courseCode;
 
+    public Enrollment(String studentId, String courseCode) {
+        this.studentId = studentId;
+        this.courseCode = courseCode;
+    }
+
+    // Getters and setters
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Enrollment{" +
+                "studentId='" + studentId + '\'' +
+                ", courseCode='" + courseCode + '\'' +
+                '}';
+    }
 }
