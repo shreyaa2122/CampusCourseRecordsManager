d153d39923d2565a686264a89bf1967f7ebdacd6
shreyaa2122 <shreya.23bcy10243@vitbhopal.ac.in>
2025-09-25 16:24:58 +0530

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,5 +1,60 @@
 package edu.ccrm.domain;
 
 public class Course {
+    private final String code;
+    private final String title;
+    private final int credits;
+    private final String instructor;
+    private final String semester;
+    private final String department;
 
+    private Course(Builder builder) {
+        this.code = builder.code;
+        this.title = builder.title;
+        this.credits = builder.credits;
+        this.instructor = builder.instructor;
+        this.semester = builder.semester;
+        this.department = builder.department;
+    }
+
+    public String getCode() { return code; }
+    public String getTitle() { return title; }
+    public int getCredits() { return credits; }
+    public String getInstructor() { return instructor; }
+    public String getSemester() { return semester; }
+    public String getDepartment() { return department; }
+
+    @Override
+    public String toString() {
+        return "Course Details:\n" +
+               "----------------------------------\n" +
+               "Code: " + this.code + "\n" +
+               "Title: " + this.title + "\n" +
+               "Credits: " + this.credits + "\n" +
+               "Instructor: " + this.instructor + "\n" +
+               "Semester: " + this.semester + "\n" +
+               "Department: " + this.department + "\n" +
+               "----------------------------------";
+    }
+
+    public static class Builder {
+        private String code;
+        private String title;
+        private int credits;
+        private String instructor;
+        private String semester;
+        private String department;
+
+        public Builder code(String code) { this.code = code; return this; }
+        public Builder title(String title) { this.title = title; return this; }
+        public Builder credits(int credits) { this.credits = credits; return this; }
+        public Builder instructor(String instructor) { this.instructor = instructor; return this; }
+        public Builder semester(String semester) { this.semester = semester; return this; }
+        public Builder department(String department) { this.department = department; return this; }
+
+        public Course build() {
+            return new Course(this);
+        }
+    }
 }
+
