fdccf50140154b8a62ff950cd0435581b4e57e29
shreyaa2122 <shreya.23bcy10243@vitbhopal.ac.in>
2025-09-25 18:13:37 +0530

    Update MaxCreditLimitExceededException.java

--- MaxCreditLimitExceededException.java
+++ MaxCreditLimitExceededException.java
@@ -1,5 +1,11 @@
 package edu.ccrm.exception;
 
-public class MaxCreditLimitExceededException {
+import java.io.Serializable;
 
+public class MaxCreditLimitExceededException extends Exception implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public MaxCreditLimitExceededException(String message) {
+        super(message);
+    }
 }
