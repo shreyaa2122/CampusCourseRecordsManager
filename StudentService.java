9c289545ff03e94a17acab917c61c3a0d2c5fef6
shreyaa2122 <shreya.23bcy10243@vitbhopal.ac.in>
2025-09-25 18:03:43 +0530

    Update StudentService.java

--- StudentService.java
+++ StudentService.java
@@ -1,5 +1,27 @@
 package edu.ccrm.service;
 
-public class StudentService {
+import edu.ccrm.domain.Student;
+import edu.ccrm.domain.StudentStatus;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+public class StudentService {
+    private List<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        students.add(student);
+        System.out.println("Student " + student.getFullName() + " added successfully.");
+    }
+
+    public List<Student> getAllStudents() {
+        return new ArrayList<>(students);
+    }
+
+    public Optional<Student> findStudentById(String id) {
+        return students.stream()
+                .filter(s -> s.getId().equals(id))
+                .findFirst();
+    }
 }
+
